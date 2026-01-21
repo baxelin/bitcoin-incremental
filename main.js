9af9a824fb6da4e336e0fa38894f4d698583b56e
Lucas Baccelli <94851988+baxelin@users.noreply.github.com>
2026-01-21 16:13:33 -0300

    refactor: refactor the code for a better reading

--- main.js
+++ main.js
@@ -1,44 +1,81 @@
-const bitcoinsTotal = document.getElementById('bitcoins-total');
-const minerarButton = document.getElementById('minerar-botao').addEventListener('click', increaseBitcoins);
-const valorReal = document.getElementById('valor-real');
-const venderBTC = document.getElementById("vender-btc").addEventListener('click', venderBitcoins);
-const upgradeClick = document.getElementById('upgrade-click');
-const clickPowerText = document.getElementById('click-power-text');
+/* ==================================== */
+/* Global DOMs */
+/* ==================================== */
+const bitcoinsTotal = document.getElementById('bitcoins');
+const moneyTotal = document.getElementById('money');
+const btnMiner = document.getElementById('mine-button');
+const btnSell = document.getElementById('sell-button');
+const clickPower = document.getElementById('click-power');
+const buyClickPower = document.getElementById('buy-click-power');
 
-upgradeClick.addEventListener('click', upgradeIncremental);
-
+/* ==================================== */
+/* Global Variables */
+/* ==================================== */
+bitcoinsTotal.innerHTML = "0.00000000 BTC";
+moneyTotal.innerHTML = "R$ 0.00";
 let satoshis = 0;
 let real = 0;
-let btc = 0;
-let upgradeManual = 5;
-let clickPower = 1;
+let clickPowerValue = 1;
+let ClickPowerPrice = 5;
 
-bitcoinsTotal.innerHTML = "0.00000000" + " BTC";
-valorReal.innerHTML = "R$ " + real;
 
-function increaseBitcoins() {
-    satoshis += 1 * clickPower;
-    btc = satoshis / 100000000;
-    btc = btc.toFixed(8);
-    bitcoinsTotal.innerHTML = btc + " BTC";
+/* ==================================== */
+/* Render BTC and Real */
+/* ==================================== */
+function renderBTC () {
+    const btcNumber = satoshis / 100_000_000;
+    const btcText = btcNumber.toFixed(8);
+    bitcoinsTotal.textContent = `${btcText} BTC`;
 };
 
-function venderBitcoins () {
-    real = satoshis / 3 + real;
-    valorReal.innerHTML = "R$ " + real.toFixed(2);
-    satoshis = 0;
-    btc = 0;
-    bitcoinsTotal.innerHTML = "0.00000000" + " BTC";
+function renderReal () {
+    const realNumber = real;
+    const realText = realNumber.toFixed(2);
+    moneyTotal.textContent = `R$ ${realText}`
 };
 
-function upgradeIncremental () {
-    if (real >= upgradeManual) {
-        real -= upgradeManual;
-        valorReal.innerHTML = "R$ " + real.toFixed(2);
-        clickPower = clickPower * 1.1;
-        upgradeManual = upgradeManual * 1.1;
-        upgradeManual = upgradeManual.toFixed(2)
-        upgradeClick.innerHTML = "Upgrade Click! = R$ " + upgradeManual;
-        clickPowerText.innerHTML = "Click Power = " + clickPower.toFixed(2);
-    }
+function renderPowerPrice () {
+    const powerPriceNumber = ClickPowerPrice;
+    const powerPriceText = powerPriceNumber.toFixed(2);
+    buyClickPower.textContent = `Buy Click Power = R$ ${powerPriceText}`
 }
+
+/* ==================================== */
+/* Manual CLicker */
+/* ==================================== */
+function mineBTC() {
+    satoshis += 1 * clickPowerValue;
+    renderBTC();
+};
+
+btnMiner.addEventListener('click', mineBTC);
+
+/* ==================================== */
+/* Sell Bitcoins */
+/* ==================================== */
+function sellBTC() {
+    real = satoshis / 3 + real;
+    satoshis = 0;
+    btcNumber = 0;
+    renderBTC();
+    renderReal();
+}
+
+btnSell.addEventListener('click', sellBTC);
+
+/* ==================================== */
+/* CLicker Upgrade */
+/* ==================================== */
+function upgradeClick(){
+    if (real >= ClickPowerPrice) {
+        real -= ClickPowerPrice;
+        renderReal();
+        renderPowerPrice();
+    }
+};
+
+buyClickPower.addEventListener('click', upgradeClick);
+
+/* ==================================== */
+/* AutoClickers */
+/* ==================================== */
